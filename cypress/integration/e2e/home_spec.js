d1b4e00a1d8e091cc4d9f5ceda93e6a48918923a
tux7P <tux7P@users.noreply.github.com>
2022-05-16 06:44:47 +0530

    Add E2E tests

---
+++ cypress/integration/e2e/home_spec.js
@@ -0,0 +1,21 @@
+describe('Verify Nav Menu', function() {
+    it('Verify Nav', function() {
+     
+        cy.visit("/")
+        cy.contains('Use Cases').click()
+        cy.location('pathname').should('eq', '/use-cases')
+        cy.go('back')
+     
+        cy.contains('Pricing').click()
+        cy.location('pathname').should('eq', '/pricing')
+        cy.go('back')
+     
+        cy.contains('About Us').click()
+        cy.location('pathname').should('eq', '/about-us')
+        cy.go('back')
+    })
+    
+    it('', function() {
+     
+    }) 
+})
