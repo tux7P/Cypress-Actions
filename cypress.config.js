dc21b9e7c16b370671fe1460d1369cde30e4fde3
tux7P <tux7P@users.noreply.github.com>
2022-06-10 21:04:40 -0300

    Fixing performance test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require('cypress')
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
 
 module.exports = defineConfig({
-  fixturesFolder: false,
+  //fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
-    supportFile: false,
-    baseUrl: 'https://www.tango.us',
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+      
+      on('task', {
+        lighthouse: lighthouse(),
+      });
+      return null
+    },
+  //  supportFile: false,
+  //  baseUrl: 'https://www.tango.us',
+    
   },
 })
